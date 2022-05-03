5beeafeb036a3d74ad75392aaa920ae88f6b6c80
Pablo-Hormazabal <cpablof@hotmail.com>
2022-05-03 11:46:34 -0300

    cors4

--- src/main/java/com/portfolio/backend/security/controller/AuthController.java
+++ src/main/java/com/portfolio/backend/security/controller/AuthController.java
@@ -28,7 +28,7 @@ import java.util.Set;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://portfolio-pablo-hormazab-6bd82.web.app")
 public class AuthController {
 
     @Autowired
